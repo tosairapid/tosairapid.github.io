ebf3c213b00c4ebced802b022b0f205be84475aa
tana <113218151+tosairapid@users.noreply.github.com>
2025-01-03 18:41:55 +0900

    Update Lottery.js

--- metroroulette/js/Lottery.js
+++ metroroulette/js/Lottery.js
@@ -3,8 +3,8 @@ let config = {};
 
 // JSONファイルを読み込む
 Promise.all([
-  fetch("stations.json").then(response => response.json()),
-  fetch("config.json").then(response => response.json())
+  fetch("../json/stations.json").then(response => response.json()),
+  fetch("../json/config.json").then(response => response.json())
 ])
   .then(([stationsData, configData]) => {
     stations = stationsData;
